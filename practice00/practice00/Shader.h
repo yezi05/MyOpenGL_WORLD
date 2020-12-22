e81dc6b3e7499151b2b336676fc1ab7b76efda76
yezi <1051877926@qq.com>
2020-12-22 11:46:23 +0800

    Add Shader Class

--- practice00/practice00/Shader.h
+++ practice00/practice00/Shader.h
@@ -11,8 +11,11 @@ class Shader
 public:
 	unsigned int ID;
 
-	Shader();
+	Shader(const GLchar* vertexPath, const GLchar* fragmentPath);
 
-
+	void use();
+	void setBool(const std::string& name, bool value)const;
+	void setInt(const std::string& name, int value)const;
+	void setFloat(const std::string& name, float value)const;
 };
 
